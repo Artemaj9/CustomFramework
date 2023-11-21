d80d8829e8a2b8e8c285539b343e89a855d9c6a5
Artem Moiseev <moiseev@physics.msu.ru>
2023-11-21 13:42:19 +0300

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Artem on 21.11.2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work")
+    }
+}
